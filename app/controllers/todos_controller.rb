207841289bb60fcab7478d454e12fce441011d2b
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-07 18:43:59 +0200

    controller created

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
   #GET /todos
   #GET /todos.json
   def index
@@ -7,4 +7,15 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def todo_params
+      params.require(:todo).permit(:title, :completed)
+    end
+    
 end
